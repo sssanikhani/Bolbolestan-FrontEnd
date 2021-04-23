50be9d7036fff41f947e66d344762c939e15cf27
Sajjad <sssanikhani@gmail.com>
2021-04-23 19:17:23 +0430

    fix: true import images from static directory

--- src/Header.js
+++ src/Header.js
@@ -2,65 +2,52 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 import './common.css';
 import React from 'react';
+import logo from './static/SRC/logo.png';
+import logoutLogo from './static/SRC/001-log-out.png'
 
 class Header extends React.Component {
 
-    render() {
-        const page = this.props.page;
-        const isLoggedIn = this.props.isLoggedIn;
-        let links;
-        if (page === 'plan') {
-            links = (
-                <React.Fragment>
-                    <Link to="/home">خانه</Link>
-                    <Link to="/courses">انتخاب واحد</Link>
-                </React.Fragment>
-            );
-        }
-        else if (page === 'courses') {
-            links = (
-                <React.Fragment>
-                    <Link to="/home">خانه</Link>
-                    <Link to="/plan">برنامه هفتگی</Link>
-                </React.Fragment>
-            );
-        }
-        else {
-            links = (
-                <React.Fragment>
-                    <Link to="/courses">انتخاب واحد</Link>
-                    <Link to="/plan">برنامه هفتگی</Link>
-                </React.Fragment>
-            );
-        }
-
-        let leftSide;
-        if (isLoggedIn) {
-            leftSide = (
-                <div className="leftside">
-                    <span>خروج</span>
-                    <img src="../static/SRC/001-log-out.png" className="icon" />
-                </div>
-            );
-        }
-        else {
-            leftSide = (
-                <div className="leftSide">
-                        <Link to="/login">ورود</Link>
-                </div>
-            );
-        }
-
-        return (
-            <header className="fixed-top">
-                <div className="rightside">
-                    <img className="logo" src="../static/SRC/logo.png" />
-                    {links}
-                </div>
-                {leftSide}
-            </header>
-        );   
+  render() {
+    const page = this.props.page;
+    let links;
+    if (page === 'plan') {
+      links = (
+        <React.Fragment>
+          <Link to="/home">خانه</Link>
+          <Link to="/courses">انتخاب واحد</Link>
+        </React.Fragment>
+      );
     }
+    else if (page === 'courses') {
+      links = (
+        <React.Fragment>
+          <Link to="/home">خانه</Link>
+          <Link to="/plan">برنامه هفتگی</Link>
+        </React.Fragment>
+      );
+    }
+    else {
+      links = (
+        <React.Fragment>
+          <Link to="/courses">انتخاب واحد</Link>
+          <Link to="/plan">برنامه هفتگی</Link>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <header className="fixed-top">
+        <div className="rightside">
+          <img className="logo" src={logo} />
+          {links}
+        </div>
+        <div className="leftside">
+          <span>خروج</span>
+          <img src={logoutLogo} className="icon" />
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
