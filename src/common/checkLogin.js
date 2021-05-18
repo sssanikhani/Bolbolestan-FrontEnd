1e904905df17f7b60426b42cc19126effdbd8204
Sajjad <sssanikhani@gmail.com>
2021-05-18 17:05:46 +0430

    feat: check localStorage before validating jwt by server

--- src/common/checkLogin.js
+++ src/common/checkLogin.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 
 async function checkLogin() {    
     let isLoggedIn = false;
+    if (localStorage.getItem("token") === null)
+        return false;
     try {
-        let response = await axios.get('http://localhost:8080/student');
+        let response = await axios.get('http://localhost:8080/student', {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+            }
+        });
         if (response.status === 200) {
             isLoggedIn = true;
         }
     } catch(error) {
+        console.log("INJA");
         console.log(error);
     }
     
