670f387c485b33e10185f35b1dcd37a93fcbe345
Sajjad <sssanikhani@gmail.com>
2021-05-27 03:51:50 +0430

    fix: start container if already exists and stopped

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ docker build -t $project_image_name .
 
 [[ $(docker ps -f "name=$project_name" --format '{{.Names}}') == $project_name ]] || 
 docker run --name $project_name -p 3000:3000 -it $project_image_name
+
+docker start $project_name
