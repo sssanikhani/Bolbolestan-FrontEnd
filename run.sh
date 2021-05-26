3502403546fcb6bdb478b588fb882f8a258ffac5
Sajjad <sssanikhani@gmail.com>
2021-05-27 03:21:18 +0430

    feat: add script file to build and run frontend container

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+project_name="bolbolestan-front"
+project_image_name="bolbolestan-front:latest"
+
+docker build -t $project_image_name .
+
+[[ $(docker ps -f "name=$project_name" --format '{{.Names}}') == $project_name ]] || 
+docker run --name $project_name -p 3000:3000 -it $project_image_name
