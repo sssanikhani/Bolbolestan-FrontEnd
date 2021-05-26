c130e2db5d8bf7ce9e8f083a62d4fd28b72e9a03
Sajjad <sssanikhani@gmail.com>
2021-05-27 04:10:56 +0430

    fix: check container exists with any status

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ project_image_name="bolbolestan-front:latest"
 
 docker build -t $project_image_name .
 
-[[ $(docker ps -f "name=$project_name" --format '{{.Names}}') == $project_name ]] || 
+[[ $(docker ps -a -f "name=$project_name" --format '{{.Names}}') == $project_name ]] || 
 docker run --name $project_name -p 3000:3000 -it $project_image_name
 
 docker start $project_name
